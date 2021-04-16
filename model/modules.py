1c87fb03b253ca2e19fbbd92ae8ad0a72d2e5ba7
Zaid Alyafeai <alyafey22@gmail.com>
2021-04-16 17:13:37 +0300

    Update modules.py

--- model/modules.py
+++ model/modules.py
@@ -38,13 +38,20 @@ class VarianceAdaptor(nn.Module):
         n_bins = model_config["variance_embedding"]["n_bins"]
         assert pitch_quantization in ["linear", "log"]
         assert energy_quantization in ["linear", "log"]
-        with open(
+        try:
+            with open(
             os.path.join(preprocess_config["path"]["preprocessed_path"], "stats.json")
         ) as f:
-            stats = json.load(f)
-            pitch_min, pitch_max = stats["pitch"][:2]
-            energy_min, energy_max = stats["energy"][:2]
-
+                stats = json.load(f)
+                pitch_min, pitch_max = stats["pitch"][:2]
+                energy_min, energy_max = stats["energy"][:2]
+        except:
+            with open(
+            os.path.join(preprocess_config["path"]["stats_path"], "stats.json")
+        ) as f:
+                stats = json.load(f)
+                pitch_min, pitch_max = stats["pitch"][:2]
+                energy_min, energy_max = stats["energy"][:2]
         if pitch_quantization == "log":
             self.pitch_bins = nn.Parameter(
                 torch.exp(
