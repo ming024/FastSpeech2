697dad4d805eb36d749560a778637335ddfd8817
Wataru-Nakata <wataru9871@gmail.com>
2021-05-10 18:49:59 +0900

    Create retrieve_transcripts.py

---
+++ retrieve_transcripts.py
@@ -0,0 +1,12 @@
+import glob
+import pandas as pd
+# %%
+transcript_files = glob.glob("/home/nakata/jsut_ver1.1_original/**/transcript_utf8.txt")
+# %%
+for transcript in transcript_files:
+    with open(transcript, mode='r') as f:
+        lines = f.readlines()
+    for line in lines:
+        filename, text = line.split(':')
+        with open('raw_data/JSUT/JSUT/' + filename + '.lab', mode='w') as f:
+            f.write(text.strip('\n')
