e4905771905ce136642e2fcbdcf5cc06ee431211
Aidan Pine <hello@aidanpine.ca>
2021-06-13 14:53:35 -0700

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -35,7 +35,7 @@ def text_to_sequence(text, cleaner_names):
             sequence += _symbols_to_sequence(_clean_text(text, cleaner_names))
             break
         sequence += _symbols_to_sequence(_clean_text(m.group(1), cleaner_names))
-        sequence += _arpabet_to_sequence(m.group(2))
+        sequence += _symbols_to_sequence(m.group(2))
         text = m.group(3)
 
     return sequence
