67368f8db73e75147448d4a851cba3e8382eda43
yuanchengwang <1057041679@qq.com>
2022-10-12 12:57:50 +0800

    fix a import bug for dataset.py

--- dataset.py
+++ dataset.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
     import torch
     import yaml
     from torch.utils.data import DataLoader
-    from utils.utils import to_device
+    from utils.tools import to_device
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     preprocess_config = yaml.load(
