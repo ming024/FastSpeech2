96c701070e777ed8b3d94014f684cf2f242b64b2
Daniel Chin <daniel.chin@nyu.edu>
2023-10-18 14:56:48 +0400

    support loading models to CPU

--- utils/model.py
+++ utils/model.py
@@ -7,6 +7,7 @@ import numpy as np
 import hifigan
 from model import FastSpeech2, ScheduledOptim
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_model(args, configs, device, train=False):
     (preprocess_config, model_config, train_config) = configs
@@ -17,7 +18,7 @@ def get_model(args, configs, device, train=False):
             train_config["path"]["ckpt_path"],
             "{}.pth.tar".format(args.restore_step),
         )
-        ckpt = torch.load(ckpt_path)
+        ckpt = torch.load(ckpt_path, map_location=device)
         model.load_state_dict(ckpt["model"])
 
     if train:
@@ -60,7 +61,7 @@ def get_vocoder(config, device):
         config = hifigan.AttrDict(config)
         vocoder = hifigan.Generator(config)
         if speaker == "LJSpeech":
-            ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar")
+            ckpt = torch.load("hifigan/generator_LJSpeech.pth.tar", map_location=device)
         elif speaker == "universal":
             ckpt = torch.load("hifigan/generator_universal.pth.tar")
         vocoder.load_state_dict(ckpt["generator"])
